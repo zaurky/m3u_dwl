bac921a62cc928c345e5fadba702aced65e1864c
zaurky <zaurky@zeb.re>
2013-10-03 10:43:02 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from os import path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+README = path.abspath(path.join(path.dirname(__file__), 'README.md'))
+desc = 'A Python util to easily download streaming videos in m3u format'
+
+setup(
+    name='m3u_dwl',
+    version='0.0.1',
+    author='Zaurky',
+    author_email='zaurky@zeb.re',
+    description=desc,
+    long_description=open(README).read(),
+    license='GPLV2',
+    url='http://github.com/zaurky/m3u_dwl',
+    packages=['m3u_dwl'],
+    install_requires=[
+        'BeautifulSoup',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
